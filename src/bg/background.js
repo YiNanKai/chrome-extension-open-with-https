0588af82abf2e3815207c49bf5f2943ee448ae58
justforuse <allenforuse@gmail.com>
2018-12-30 18:37:01 +0800

    :sparkles: add disabled icon

--- src/bg/background.js
+++ src/bg/background.js
@@ -11,20 +11,22 @@ chrome.browserAction.onClicked.addListener(function(tab) {
 
 chrome.tabs.onActivated.addListener(function({ tabId, windowId }) {
   chrome.tabs.get(tabId, function(tab) {
-    if (tab.url.indexOf('http:') === 0) {
-      chrome.browserAction.setIcon({ path: '/icons/icon_off_128.png' })
-    } else {
+    if (tab.url.indexOf('https:') === 0) {
       chrome.browserAction.setIcon({ path: '/icons/icon_on_128.png' })
+    } else if (tab.url.indexOf('chrome:') === 0) {
+      chrome.browserAction.setIcon({ path: '/icons/icon_disabled_128.png' })
+    } else {
+      chrome.browserAction.setIcon({ path: '/icons/icon_off_128.png' })
     }
-  });
+  })
 })
 
 chrome.runtime.onMessage.addListener(function(msg, sender, sendResponse) {
   if (msg.action === 'updateIcon') {
-    if (msg.value.protocol === 'http:') {
-      chrome.browserAction.setIcon({ path: '/icons/icon_off_128.png' })
-    } else {
+    if (msg.value.protocol === 'https:') {
       chrome.browserAction.setIcon({ path: '/icons/icon_on_128.png' })
+    } else {
+      chrome.browserAction.setIcon({ path: '/icons/icon_off_128.png' })
     }
   }
 })
