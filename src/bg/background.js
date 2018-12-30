90b9c821703bd70c30e57fa7db8aa3ffc6e49d12
justforuse <allenforuse@gmail.com>
2018-12-30 12:31:20 +0800

    :bug: update icon when tab changed

--- src/bg/background.js
+++ src/bg/background.js
@@ -1,30 +1,22 @@
 chrome.browserAction.onClicked.addListener(function(tab) {
   chrome.tabs.executeScript({
     code: `
-      const newLocation = window.location.href.replace(window.location.protocol, 'https:')
-      console.log(newLocation)
-      window.location.href = newLocation
+      if(window.location.protocol === 'http:') {
+        const newLocation = window.location.href.replace(window.location.protocol, 'https:')
+        window.location.href = newLocation
+      }
     `
   })
 })
 
-function updateExtensionIcon(win) {
-  if (win.location.protocol === 'http:') {
-    chrome.browserAction.setIcon({ path: '/icons/icon_off_128.png' })
-  } else {
-    chrome.browserAction.setIcon({ path: '/icons/icon_on_128.png' })
-  }
-}
-
-function getWindow() {
-  return window;
-}
 chrome.tabs.onActivated.addListener(function({ tabId, windowId }) {
-  // chrome.tabs.executeScript({
-  //   code: `
-  //   window.postMessage({ type: "FROM_PAGE", text: "Hello from the webpage!" }, "*");
-  //   `
-  // })
+  chrome.tabs.get(tabId, function(tab) {
+    if (tab.url.indexOf('http:') === 0) {
+      chrome.browserAction.setIcon({ path: '/icons/icon_off_128.png' })
+    } else {
+      chrome.browserAction.setIcon({ path: '/icons/icon_on_128.png' })
+    }
+  });
 })
 
 chrome.runtime.onMessage.addListener(function(msg, sender, sendResponse) {
